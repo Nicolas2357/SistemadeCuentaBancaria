7dc4ca2ab09a0bf89638d1aa75f195b995fdde5c
Nicolas Ponce Carmona <poncecarmonanicolas09@gmail.com>
2026-02-04 19:20:17 -0600

    Segundo commit - Se agregan las clases Persona y Cliente

--- SistemadeCuentaBancaria/Cliente.cs
+++ SistemadeCuentaBancaria/Cliente.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace SistemadeCuentaBancaria
 {
-    internal class Cliente
+    public class Cliente : Persona
     {
+        private string _direccion;
+
+        public string Direccion
+        {
+            get { return _direccion; }
+            set { _direccion = value; }
+        }
+
+        public Cliente(int id, string nombre, string telefono, string direccion)
+            : base(id, nombre, telefono)
+        {
+            _direccion = direccion;
+        }
+
+        public override void MostrarInformacion()
+        {
+            Console.WriteLine($"Cliente: {Nombre} | Tel: {Telefono} | Dirección: {_direccion}");
+        }
     }
 }
--- SistemadeCuentaBancaria/Persona.cs
+++ SistemadeCuentaBancaria/Persona.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace SistemadeCuentaBancaria
 {
-    internal class Persona
+    public abstract class Persona
     {
+        private int _id;
+        private string _nombre;
+        private string _telefono;
+
+        public int Id
+        {
+            get { return _id; }
+        }
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+        public string Telefono
+        {
+            get { return _telefono; }
+            set { _telefono = value; }
+        }
+
+        protected Persona(int id, string nombre, string telefono)
+        {
+            _id = id;
+            _nombre = nombre;
+            _telefono = telefono;
+        }
+
+        public virtual void MostrarInformacion()
+        {
+            Console.WriteLine($"Nombre: {_nombre} | Teléfono: {_telefono}");
+        }
     }
 }
